a4352f8243b5fa71c3e32d3ee8d3d2cfbc439a78
Tom Rodríguez <tom.rodriguez@feast-it.com>
2025-07-27 20:24:09 +0100

    chore: added test for ApprovalModeContent

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -57,7 +57,7 @@ export const reducer = (
         selectedPattern: action.payload,
       };
     }
-    case "SET_TEXT": {
+    case ACTIONS.SET_TEXT: {
       return {
         ...state,
         textContent: action.payload,
